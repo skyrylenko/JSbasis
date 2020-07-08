43dac58898462794564aa4741917ea7e6e238dda
Kyrylenko, Serhiy (US - Hermitage) <skyrylenko@deloitte.com>
2020-07-08 18:40:21 +0300

    Done

---
+++ main.js
@@ -0,0 +1,25 @@
+'use strict';
+// console.log('Hello');
+// console.info('Here is some info');
+// console.warn('Be careful');
+// console.error('AAAAAAAAAA');
+
+// let test = 15, id = 555, age = 'vvv';
+// console.log(test);
+// console.log(id);
+// console.log(age);
+
+// const userName = prompt('What is your name?');
+// console.log(userName);
+
+
+let name = "John", message = "Hello!";
+let age = 45;
+name = prompt("What is your name?");
+age = prompt("How old are you?");
+message = `${name} is ${age} years old`;
+console.log(message);
+
+let nullVar = null;
+console.log(nullVar);
+
